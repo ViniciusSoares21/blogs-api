a1c453c65dbec8d19c5a291b85884cee76fcb371
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-01 15:01:31 -0300

    cria models para tabela Blog_posts com relacionamento 1:N

---
+++ src/models/BlogPost.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+    },
+    content: {
+      type: DataTypes.STRING,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+    },
+    published: {
+      type: DataTypes.DATE,
+    },
+    updated: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: 'blog_posts',
+    underscored: true,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      { foreignKey: 'user_id', as: 'user' });
+  };
+
+  return BlogPost;
+}
--- src/models/User.js
+++ src/models/User.js
@@ -25,5 +25,10 @@ module.exports = (sequelize, DataTypes) => {
     underscored: true,
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreignKey: 'user_id', as: 'BlogPost' });
+  };
+
   return User;
 }
