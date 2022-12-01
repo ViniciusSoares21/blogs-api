8b9786353733121a5329b616f88ce92753419066
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-01 10:31:27 -0300

    cria service para cadastar um novo usuario

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,7 +1,30 @@
+require('dotenv/config');
+const jwt = require('jsonwebtoken');
+
 const { User } = require('../models');
+
+const secret = process.env.JWT_SECRET || 'secretJWT';
+
+const jwtConfig = {
+  expiresIn: '7d',
+  algorithm: 'HS256',
+};
 
 const getByUserEmail = (email) => User.findOne({ where: { email } });
 
+const createUser = async (values) => {
+  const { displayName, email, password, image } = values;
+
+  const newUser = await User.create({ displayName, email, password, image });
+
+  const { password: _, ...userWithPassword } = newUser.dataValues;
+
+  const token = jwt.sign({ data: userWithPassword }, secret, jwtConfig);
+
+  return token;
+};
+
 module.exports = {
   getByUserEmail,
+  createUser,
 };
