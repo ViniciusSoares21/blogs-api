dcaf7e19953a895b11637d4709c8d978b87a7003
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-01 11:43:40 -0300

    cria controller  para buscar todas pessoas usuarias

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -10,6 +10,16 @@ const createUser = async (req, res) => {
   }
 };
 
+const getUser = async (_req, res) => {
+  try {
+    const users = await UserService.getUser();
+    return res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ message: 'Erro interno', error: err.message });
+  }
+};
+
 module.exports = {
   createUser,
+  getUser,
 };
