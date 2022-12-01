b4bea1df757e1965050f8c07999d3c3a84ce422b
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-01 11:42:35 -0300

    cria função para validar token

---
+++ src/middlewares/auth.js
@@ -0,0 +1,20 @@
+require('dotenv/config');
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET || 'seusecretdetoken';
+
+module.exports = async (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+
+  try {
+     jwt.verify(token, secret);
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
