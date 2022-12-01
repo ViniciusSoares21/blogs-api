83c93ec04a8bff099fcbbd6282ba956fad57bf37
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-01 11:44:38 -0300

    adiciona router para o endpoint /user que busca usuarios no banco de dados

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -8,6 +8,9 @@ const UserController = require('../controllers/users');
 const validateName = require('../middlewares/validateName');
 const validatePassword = require('../middlewares/validatePassword');
 const validateEmail = require('../middlewares/validateEmail');
+const auth = require('../middlewares/auth');
+
+router.get('/user', auth, UserController.getUser);
 
 router.post('/login', LoginController);
 
