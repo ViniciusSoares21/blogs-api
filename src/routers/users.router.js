c71cabbc6d10c88fa829252c047302c5fb2ec748
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-01 10:34:50 -0300

    adiciona router para o endpoint /user para cadastrar um novo usuario

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -3,7 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 const LoginController = require('../controllers/login');
+const UserController = require('../controllers/users');
+
+const validateName = require('../middlewares/validateName');
+const validatePassword = require('../middlewares/validatePassword');
+const validateEmail = require('../middlewares/validateEmail');
 
 router.post('/login', LoginController);
+
+router.post('/user', validateName, validatePassword, validateEmail, UserController.createUser);
 
 module.exports = router;
