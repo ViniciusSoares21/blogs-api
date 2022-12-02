a565fbe0ee20819d4595c5b8796fe6da16756676
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-02 16:33:00 -0300

    adiciona router para pesquisar title ou conteudo no endpoint /post/search

--- src/routers/blogPost.router.js
+++ src/routers/blogPost.router.js
@@ -8,6 +8,8 @@ const auth = require('../middlewares/auth');
 
 const validatePost = require('../middlewares/validatePost');
 
+router.get('/post/search', auth, BlogPost.searchPostTitle);
+
 router.post('/post', auth, validatePost, BlogPost.createPost);
 
 router.get('/post', auth, BlogPost.getPost);
