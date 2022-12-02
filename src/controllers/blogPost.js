916dd49751723f1d6bec178dd932eaed9a284b75
Vinicius Soares <viniciussoares72943@gmail.com>
2022-12-01 22:23:12 -0300

    cria controller para adicionar novo post

---
+++ src/controllers/blogPost.js
@@ -0,0 +1,27 @@
+const BlogPostService = require('../services/blogPost.services');
+const CategoryService = require('../services/category.service');
+
+const createPost = async (req, res) => {
+  try {
+    const { title, content, categoryIds } = req.body;
+
+    const { id } = req.use;
+
+    const validatCategorys = await Promise.all(categoryIds.map((category) => CategoryService
+      .getById(category)));
+
+    if (validatCategorys.includes(null)) {
+      return res.status(400).json({ message: 'one or more "categoryIds" not found' });
+    }
+
+    const post = await BlogPostService.createPost(title, content, categoryIds, id);
+
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(500).json({ message: 'Erro interno', error: err.message });
+  }
+};
+
+module.exports = {
+  createPost,
+};
